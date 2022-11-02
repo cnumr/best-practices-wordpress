99242a3af8b84b848efd8605b89dfe2db8e7ea45
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-11-02 11:21:42 +0100

    better design

--- src/components/internal-nav.js
+++ src/components/internal-nav.js
@@ -5,23 +5,38 @@ import classNames from 'classnames'
 function InternalNav({ pageContext, previous, next, className }) {
   return (
     <nav className={classNames('blog-post-nav border-t-2 pt-4', className)}>
-      <ul className="flex justify-between noliststyle">
+      <ul
+        className={classNames('grid noliststyle', {
+          'grid-cols-3': previous && next,
+          'grid-cols-2': !previous || !next,
+        })}
+      >
         {previous && (
-          <li>
-            <Link to={`${previous.frontmatter.path}`} rel="prev">
-              ← {previous.frontmatter.title}
+          <li className="text-center md:text-left">
+            <Link to={`${previous.frontmatter.path}.md`} rel="prev">
+              ←{' '}
+              <span className="hidden md:block">
+                {previous.frontmatter.title}
+              </span>
             </Link>
           </li>
         )}
-        <li>
+        <li
+          className={classNames('', {
+            'text-right': !previous,
+            'text-left': !next,
+            'text-center': next && previous,
+          })}
+        >
           <Link to="/" rel="home">
             Accueil
           </Link>
         </li>
         {next && (
-          <li>
-            <Link to={`${next.frontmatter.path}`} rel="next">
-              {next.frontmatter.title} →
+          <li className="text-center md:text-right">
+            <Link to={`${next.frontmatter.path}.md`} rel="next">
+              →{' '}
+              <span className="hidden md:block">{next.frontmatter.title}</span>
             </Link>
           </li>
         )}
