d9088f2628eda93784b4f4e059d012b93995d0fc
Renaud Heluin <renaud.heluin@novagaia.fr>
2023-01-05 14:26:22 +0000

    fix color and badge

--- src/components/markdown-display.js
+++ src/components/markdown-display.js
@@ -6,17 +6,21 @@ function MarkdownDisplay({ type = 'personna', data, pageContext }) {
   const { frontmatter, tableOfContents, html } = markdownRemark
   return (
     <section>
-      <h1>
-        <span className="badge bg-primary-600 mr-2 text-2xl">
-          BP {frontmatter.title.slice(0, 4)}
-        </span>
-        <span>{frontmatter.title.slice(5)}</span>
-      </h1>
+      {type === 'fiches' ? (
+        <h1 className="flex items-center">
+          <span className="badge bg-primary-500 mr-2 text-2xl whitespace-nowrap">
+            BP {frontmatter.title.slice(0, 4)}
+          </span>
+          <span>{frontmatter.title.slice(5)}</span>
+        </h1>
+      ) : (
+        <h1>{frontmatter.title}</h1>
+      )}
 
-      <i>
+      <span className="text-primary-500 font-bold">
         Mise à jour le{' '}
         {new Date(fields?.gitUpdateTime).toLocaleDateString('fr')}
-      </i>
+      </span>
       {type === 'fiches' && (
         <FichesMetasDisplay
           frontmatter={frontmatter}
