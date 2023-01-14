0a4abda2cb2937a1c51fa49d3059b304c7447ded
Florine Sueur <florine.sueur@outlook.fr>
2023-01-14 16:17:17 +0000

    add letters anchor and clean lexique

--- src/pages/lexique.js
+++ src/pages/lexique.js
@@ -1,9 +1,16 @@
-import { Card, Layout, Seo } from '../components'
+import { Layout, Seo } from '../components'
 
 import React from 'react'
 import { graphql } from 'gatsby'
 
 function Lexique({ data, location }) {
+  console.log('data.lexiqueEntries.nodes :>> ', data.lexiqueEntries.nodes)
+
+  const byGroup = data.lexiqueEntries.nodes.reduce((r, a) => {
+    r[a.name.slice(0, 1)] = [...(r[a.name.slice(0, 1)] || []), a]
+    return r
+  }, [])
+
   return (
     <Layout>
       <Seo
@@ -13,34 +20,52 @@ function Lexique({ data, location }) {
       />
       <main>
         <h1>Lexique</h1>
+        <ul className="flex gap-2 flex-wrap items-start mb-2">
+          {Object.keys(byGroup).map((entry, index) => {
+            return (
+              <li
+                key={`button-${index}-${entry}`}
+                className="list-none whitespace-nowrap m-0 leading-none mb-4"
+              >
+                <a
+                  href={`#${entry}`}
+                  className="badge text-sm transition hover:bg-primary-transparent text-neutral-DEFAUT no-underline"
+                >
+                  {entry}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
         <ul>
-          {/* {data.lexiqueEntries.nodes.map((entry, index) => {
-            if (entry.childMarkdownRemark?.frontmatter.path)
-              return (
-                <Card
-                  key={index}
-                  type={Card.LEXIQUE}
-                  markdownRemark={entry.childMarkdownRemark}
-                />
-              )
-            else return null
-          })} */}
-          {data.lexiqueEntries.nodes.map((entry, index) => {
-            if (entry.childMarkdownRemark?.frontmatter.path)
-              return (
-                <li className="box pb-0 mb-6 border-neutral-transparent">
-                  <h2 className="text-neutral-DEFAUT mt-0 mb-4">
-                    {entry.childMarkdownRemark?.frontmatter.title}
-                  </h2>
-                  <div
-                    className="markdown-content"
-                    dangerouslySetInnerHTML={{
-                      __html: entry.childMarkdownRemark?.html,
-                    }}
-                  />
-                </li>
-              )
-            else return null
+          {Object.entries(byGroup).map((entry, index) => {
+            console.log('entry[0] :>> ', entry[0])
+            return (
+              <li key={`lexique-${index}-${entry[0]}`}>
+                <ul id={`${entry[0]}`}>
+                  {entry[1].map(({ childMarkdownRemark }, index) => {
+                    if (childMarkdownRemark.frontmatter.path)
+                      return (
+                        <li
+                          className="box pb-0 mb-6 border-neutral-transparent"
+                          key={`${index}-${childMarkdownRemark?.frontmatter.path}`}
+                        >
+                          <h2 className="text-neutral-DEFAUT mt-0 mb-4">
+                            {childMarkdownRemark?.frontmatter.title}
+                          </h2>
+                          <div
+                            className="markdown-content"
+                            dangerouslySetInnerHTML={{
+                              __html: childMarkdownRemark?.html,
+                            }}
+                          />
+                        </li>
+                      )
+                    else return null
+                  })}
+                </ul>
+              </li>
+            )
           })}
         </ul>
       </main>
@@ -62,6 +87,7 @@ export const query = graphql`
     ) {
       nodes {
         id
+        name
         childMarkdownRemark {
           id
           frontmatter {
